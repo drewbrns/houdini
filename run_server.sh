03dcbce70441f755da06e09a8b626f4b944a7dfd
Drew Barnes <djblin@gmail.com>
2018-03-28 11:34:20 +0000

    Fix run_server.sh script

--- run_server.sh
+++ run_server.sh
@@ -2,4 +2,4 @@
 
 export NLTK_DATA="/app/bin/nltk_data/"
 
-(cd app/playground; jupyter notebook --notebook-dir='/app/playground' --ip=0.0.0.0 --allow-root)D
+(jupyter notebook --notebook-dir='/app/playground' --ip=0.0.0.0 --allow-root)
