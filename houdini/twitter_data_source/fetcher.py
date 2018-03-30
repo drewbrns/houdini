8e17fa5c86260c4322c37fadc0a72c8a783a16fb
Drew Barnes <djblin@gmail.com>
2018-03-30 20:04:43 +0000

    comment out logs

--- houdini/twitter_data_source/fetcher.py
+++ houdini/twitter_data_source/fetcher.py
@@ -18,7 +18,8 @@ class TweetFetcher(TwythonStreamer):
             )
             print ('[TweetFetcher] Tweet collected at: {}'.format(str(data['created_at'])))
         except Exception as e:
-            print(e)
+            # print(e)
+            pass 
     
     def on_error(self, status_code, data):
         raise (status_code, data)
